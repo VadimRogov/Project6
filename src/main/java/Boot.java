31e50c849a04e198f5d362f635aac7517b4db104
Bear <world.life.destiny@icloud.com>
2023-12-09 17:18:15 +0300

    Project6

---
+++ src/main/java/Boot.java
@@ -0,0 +1,46 @@
+import comparator.StudentComparator;
+import comparator.UniversityComparator;
+import enums.StudentComparatorType;
+import enums.UniversityComparatorType;
+import io.XlsReader;
+import io.XlsWriter;
+import model.Statistics;
+import model.Student;
+import model.University;
+import util.ComparatorUtil;
+import util.JsonUtil;
+import util.StatisticsUtil;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import static java.util.logging.Level.INFO;
+
+public class Boot {
+    private static final Logger logger = Logger.getLogger(Boot.class.getName());
+    public static void main(String[] args) throws IOException {
+
+        try {
+            LogManager.getLogManager().readConfiguration(
+                    Boot.class.getResourceAsStream("/logging.properties"));
+        }catch(IOException e) {
+            System.out.println("Could not setup logger configuration: " + e.toString());
+        }
+
+        logger.log(INFO, "Application started, Logger configured");
+
+        List<University> universities = XlsReader.readXlsUniversities("src/main/resources/universityInfo.xlsx");
+        UniversityComparator universityComparator = ComparatorUtil.getUniversityComparator(UniversityComparatorType.YEAR);
+
+        List<Student> students = XlsReader.readXlsStudents("src/main/resources/universityInfo.xlsx");
+        StudentComparator studentComparator = ComparatorUtil.getStudentComparator(StudentComparatorType.AVG_EXAM_SCORE);
+
+        List<Statistics> statisticsList = StatisticsUtil.createStatistics(students, universities);
+        XlsWriter.writeXlsStatistics(statisticsList, "statistics.xlsx");
+
+        logger.log(INFO, "Application finished");
+    }
+}
